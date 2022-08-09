820deac3ce05203c563dd01490bfaf422979b171
CarlosMCuadrado <carlos.cm@telefonica.net>
2022-08-09 09:41:47 +0200

    Finished styles & basic site config

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,12 +2,16 @@ import * as React from 'react'
 
 import Header from '../components/header'
 import Footer from '../components/footer'
+//import '../styles/index.scss'
+import * as layoutStyles from './layout.module.scss'
 
 const Layout = (props) => {
     return (
-        <div>
+        <div className={layoutStyles.container}>
+            <div className={layoutStyles.content}>
             <Header />
             {props.children}
+            </div>         
             <Footer />
         </div>
     )
