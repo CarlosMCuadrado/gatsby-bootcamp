6e7cf6ab25fdef7d8c00d1131668ce0e21c5a4fa
CarlosMCuadrado <carlos.cm@telefonica.net>
2022-09-25 19:03:14 +0200

    Image contentful support added

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -37,6 +37,16 @@ query($slug: String!) {
     publishedDate(formatString: "MMMM Do, YYYY")
     body {
       raw
+      references {
+        ...on ContentfulAsset {
+          contentful_id
+          __typename
+          file {
+            url
+            fileName
+          }
+        }
+      }
     }
   }
 }
@@ -44,15 +54,28 @@ query($slug: String!) {
 
 
 
-const Blog = (props) => {
+const Blog = ({ data }) => {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return (
+            <img
+              src={node.data.target.file.url}
+              alt={node.data.target.file.fileName}
+            />
+        )
+      },
+    },
+  }
 
-    return (
-        <Layout>
-            <h1>{props.data.contentfulBlogPost.title}</h1>
-            <p>{props.data.contentfulBlogPost.publishedDate}</p>
-            {renderRichText(props.data.contentfulBlogPost.body)}
-        </Layout>
-    )
-    }
+  return (
+    <Layout>
+      <h1>{data.contentfulBlogPost.title}</h1>
+      <p>{data.contentfulBlogPost.publishedDate}</p>
+      {data.contentfulBlogPost.body &&
+        renderRichText(data.contentfulBlogPost.body, options)}
+    </Layout>
+  )
+}
 
 export default Blog
